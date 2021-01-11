5ba055d2ed9046525b128f2a83eeb0d54b048b5e
Ida Adjivon <iadjivon@bu.edu>
2021-01-11 00:52:54 -0500

    created component folder

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1> Nassoma </h1>
+      <p> this is a paragraph</p>
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+<h2> this is the header page </h2>
