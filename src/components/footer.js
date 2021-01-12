6573fd179f9a9bab226fdac57f65bdc5c21d8892
Ida Adjivon <iadjivon@bu.edu>
2021-01-12 00:05:58 -0500

    footer page created

---
+++ src/components/footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+<div>
+    <h1> this is the footer</h1>
+    <div>
+        <p> this is the first inner div </p>
+    </div>
+</div>
--- src/components/header.js
+++ src/components/header.js
@@ -2,4 +2,4 @@ import React from 'react'
 
 <div> 
 <h2> this is the header page </h2>
-<p> this is a pahragph </p></div>
+</div>
