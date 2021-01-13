312751c6baa70be8882f4a28090c13d022e4eab4
Ida Adjivon <iadjivon@bu.edu>
2021-01-13 00:05:15 -0500

    added a bdy component

---
+++ src/components/body.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+<div>
+    <div> 
+    <h1>
+        this is a content of the main page 
+    </h1>
+    </div>
+
+    <div> 
+    <h1>
+        this is another content of the main page 
+    </h1>
+    </div>
+
+    <div> 
+    <h1>
+        this is a third content of the main page 
+    </h1>
+    </div>
+</div>
