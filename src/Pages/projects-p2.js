5aec15af954b9e4214700bc7e4ba0a4ace222e66
Ida Adjivon <iadjivon@bu.edu>
2021-01-13 00:32:55 -0500

    edits to the projects page

--- src/Pages/projects-p2.js
+++ src/Pages/projects-p2.js
@@ -5,5 +5,8 @@ import React from 'react'
         this is the page for the second project. it is similar to the first except for its content.
     </h1>
 
-   
+    <p>
+        Is there any other way to display this information?
+        a need for a backend maybe?
+    </p>
 </div>
