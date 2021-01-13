3e1b893a72f6fdae914d319870c0e7e36bf4619b
Ida Adjivon <iadjivon@bu.edu>
2021-01-13 00:22:14 -0500

    pages are identified

--- src/Pages/projects-all.js
+++ src/Pages/projects-all.js
@@ -1,1 +1,7 @@
 import React from 'react'
+
+<div>
+    <h1> 
+        this is the page that has all projects 
+    </h1>
+</div>
--- src/Pages/projects-p1.js
+++ src/Pages/projects-p1.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 <div>
     <h1> 
-        this is the main landing page
+        this is the page for the first project 
     </h1>
 </div>
